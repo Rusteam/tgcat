14daf5adf66aa0458967911d583c8544ef854c02
Rustem.Galiullin <rustem.galiullin@rustem.galiullin>
2021-04-20 19:28:17 +0400

    use new input files

--- src/run_cmd.sh
+++ src/run_cmd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./tgcat-tester language data/external/dc0415-input-all.txt data/processed/dc0415-language_output.txt
-./tgcat-tester category data/external/dc0415-input-all.txt data/processed/dc0415-category_output.txt
+./tgcat-tester language data/external/dc0415-input/original/dc0415-input-all.txt data/processed/dc0415-language_output.txt
+./tgcat-tester category data/external/dc0415-input/original/dc0415-input-all.txt data/processed/dc0415-category_output.txt
