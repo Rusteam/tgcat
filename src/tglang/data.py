77def30d4f54c3168c50b52a7094926ecd50a849
Rustem Galiullin <rustem.galiullin@bayanat.ai>
2023-10-13 22:06:58 +0400

    pull data and train a model

---
+++ src/tglang/data.py
@@ -0,0 +1,72 @@
+"""Download a dataset from HuggingFace Datasets.
+"""
+from pathlib import Path
+
+import fire
+from datasets import load_dataset
+from tqdm.auto import tqdm
+
+lang_maps = dict(
+    docker="dockerfile",
+    apache_groovy="groovy",
+    basic="realbasic",
+    batch="batchfile",
+    coffescript="coffeescript",
+    common_lisp="common_lisp",
+    cplusplus="c++",
+    protobuf="protocol_buffer",
+    objective_c="objective_c++",
+    lisp="emacs_lisp",
+
+
+)
+exclude = [
+    "other", "1s_enterprise", "actionscript",
+    "apex", "csharp", "delphi", "fift",
+    "func", "hack"
+]
+
+
+def download_thestack(dest_dir="./data/raw/thestack",
+                      n_samples=10):
+    lang_enums = Path(__file__).with_name("langs_enum.txt").read_text().splitlines()
+    dest_path = Path(__file__).parents[2] / dest_dir
+    progress_bar = tqdm(lang_enums, desc="downloading languages")
+
+    for lang in lang_enums[::-1]:
+        req_lang = lang.split("_", maxsplit=2)[-1].lower()
+        progress_bar.set_description(f"downloading {req_lang}")
+        progress_bar.update(1)
+
+        class_dir = dest_path / lang
+        class_dir.mkdir(parents=True, exist_ok=True)
+
+        if req_lang in exclude:
+            continue
+        elif req_lang in lang_maps:
+            req_lang = lang_maps[req_lang]
+
+        n_exist = len(list(class_dir.glob("*.txt")))
+        if n_exist >= n_samples:
+            continue
+
+        try:
+            ds = load_dataset(
+                "bigcode/the-stack",
+                data_dir=f"data/{req_lang}",
+                split="train",
+                streaming=True
+            )
+
+            if n_exist > 0:
+                ds = ds.skip(n_exist)
+            for idx, sample in enumerate(iter(ds.take(n_samples - n_exist))):
+                dest_file = class_dir / f"{idx+n_exist}.txt"
+                dest_file.write_text(sample["content"])
+
+        except Exception as e:
+            print(f"Failed to download {req_lang}: {e}")
+
+
+if __name__ == "__main__":
+    fire.Fire(download_thestack)
